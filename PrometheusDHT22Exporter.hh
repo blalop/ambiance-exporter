60d0dea4aa2a2bbf3c755cb328dddf3711df3d03
Alejandro Blanco <alexbl1996@gmail.com>
2021-08-28 17:38:48 +0200

    config extracted to header

--- PrometheusDHT22Exporter.hh
+++ PrometheusDHT22Exporter.hh
@@ -1,4 +1,15 @@
 #pragma once
 
-const char* ssid = "yourssid";
-const char* password = "yourpass";
+#include <DHT.h>
+
+// Http Server config
+const String SSID = "yourssid";
+const String PASS = "yourpass";
+const uint16_t PORT = 80;
+
+// DHT config
+const uint8_t DHTPIN = D2;
+const uint8_t DHTTYPE = DHT22;  // DHT 22  (AM2302), AM2321
+
+// Project specific config
+const String METRIC_PREFIX = "esp8266";
--- PrometheusDHT22Exporter.ino
+++ PrometheusDHT22Exporter.ino
@@ -1,63 +1,53 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <DHT.h>
-
 #include "PrometheusDHT22Exporter.hh"
 
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#include <DHT.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 
+ESP8266WebServer server(PORT);
+DHT dht(DHTPIN, DHTTYPE);
 
-ESP8266WebServer server(80);
-
-uint8_t DHTPin = D2; 
-               
-DHT dht(DHTPin, DHTTYPE);                
-
-float Temperature;
-float Humidity;
- 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
-  
-  pinMode(DHTPin, INPUT);
+    Serial.begin(115200);
 
-  dht.begin();              
+    pinMode(DHTPIN, INPUT);
 
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
+    dht.begin();
 
-  WiFi.begin(ssid, password);
+    Serial.println("Connecting to " + SSID);
 
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+    WiFi.begin(SSID, PASS);
 
-  server.on("/", onConnectHandler);
-  server.onNotFound(onNotFoundHandler);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+    }
 
-  server.begin();
-  Serial.println("HTTP server started");
+    Serial.println();
+    Serial.print("WiFi connected. Got IP: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", onConnectHandler);
+    server.onNotFound(onNotFoundHandler);
+
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 void loop() {
-  server.handleClient();
+    server.handleClient();
 }
 
 void onConnectHandler() {
-  Temperature = dht.readTemperature();
-  Humidity = dht.readHumidity();
-  server.send(200, "text/plain", buildMetrics(Temperature,Humidity)); 
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    server.send(200, "text/plain", buildMetrics(temperature, humidity));
 }
 
-void onNotFoundHandler(){
-  server.send(404, "text/plain", "Not found");
+void onNotFoundHandler() {
+    server.send(404, "text/plain", "Not found");
 }
 
 String buildMetrics(float temperature, float humidity) {
-  return "esp8266_temperature " + String(temperature) + "\r\n" + "esp8266_humidty " + String(humidity);
+    return METRIC_PREFIX + "_temperature " + String(temperature) + "\r\n" +
+           METRIC_PREFIX + "_humidty " + String(humidity);
 }
